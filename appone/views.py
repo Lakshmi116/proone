47e7e9bb1c79a76e5af9b0273206f2364161ad8f
Lakshmi116 <66024506+Lakshmi116@users.noreply.github.com>
2021-02-25 15:10:19 +0530

    commit 1

--- appone/views.py
+++ appone/views.py
@@ -2,17 +2,36 @@ from django.shortcuts import render
 from django_tex.shortcuts import render_to_pdf
 from django_tex.core import compile_template_to_pdf
 from django.shortcuts import redirect
+from appone.tex_generator import createTextFile
 import os
+from pathlib import Path
 
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 # Create your views here.
 def index(request):
     index_dict = {
         'insert_me':'text in index inserted!',
     }
     if(request.method == "POST"):
-        os.system("cd")
-        os.system("pdflatex test.tex")
-        os.system("move test.pdf ./static/pdfs")
+        """
+        createTextFile(name="Nikitha",rollno="190102052",stream="Btech",branch="ECE",minor="CSE",college="IITG",
+              email="nikithareddy@gmail.com",iitgmail="m.nikitha@iitg.ac.in",mobileno="9848670705",
+              linkedin="linkedin.com/in/nikitha2309",
+              education=[["d1","c1","p1","y1"],["d2","c2","p2","y2"],["","","",""]],
+              projects=[["title1","club1","desc1","link1","date1"],["title2","club2","desc2","link2","date2"],["title3","club3","desc3","link3","date3"],["title4","club4","desc4","link4","date4"]],
+              techskills=["pllang","webtech","dbms","os","miscell","otherskills"],
+              keyCourses=["ma101","webd101","cpp110"],
+              por=[["title1","desc1"],["title2","desc2"],["title3","desc3"],["title4","desc4"]],
+              achievements=[["title1","desc1"],["title2","desc2"],["title3","desc3"],["title4","desc4"],["title5","desc5"],["title6","desc6"]]) 
+            """    
+
+        #os.system("pdflatex test.tex")
+        #os.system("move test.pdf ./static/pdfs")
+        os.system("cd ./..")
+        os.system("python new.py")
+        os.system("pdflatex latexFile.tex")
+        os.system("move latexFile.pdf ./static/pdfs")
         return redirect('/page1/')
     return render(request,'index/index.html',context =index_dict)
 def page1(request):
